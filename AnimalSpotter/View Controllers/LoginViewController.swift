9916e7126c10f29b0205ca1003ac0d996ad24f60
Claudia Maciel <clc_80@yahoo.com>
2020-03-16 20:32:11 -0700

    API connected to login

--- AnimalSpotter/View Controllers/LoginViewController.swift
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -35,9 +35,60 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         // perform login or sign up operation based on loginType
+        guard let apiController = apiController else { return }
+        
+        // grab the data from the textfields and make sure they have at least one character
+        guard let username = usernameTextField.text, username.isEmpty == false,
+            let password = passwordTextField.text, password.isEmpty == false else {
+                return
+        }
+        // build the user object
+        let user = User(username: username, password: password)
+        
+        // are we sign in mode or sign up mode
+        switch loginType {
+        case .signUp:
+            apiController.signUp(with: user) { (error) in
+                guard error == nil else {
+                    print("Error signing up: \(error!)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Sigun Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    
+                    self.present(alertController, animated: true) {
+                        self.loginType = .signIn
+                        self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                        self.signInButton.setTitle("Sign In", for: .normal)
+                    }
+                }
+                
+            }
+        case .signIn:
+            apiController.signIn(with: user) { (error) in
+                guard error == nil else {
+                    print("Error login in: \(error!)")
+                    return
+                }
+                
+                // if it succeeds we want to dismiss the view controller
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
         // switch UI between login types
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
     }
 }
